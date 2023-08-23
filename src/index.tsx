902f3d27925cc14608210943eb914e2489d315cd
zakialvi-e64 <zaki.alvi@enterprise64.com>
2023-08-23 14:19:01 +0500

    folder 7 completed

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,60 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
+import React , {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+
+
+
+
+const App = () => {
+
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+
+  useEffect(()=> {
+    startService()
+  },[]);
+
+ 
+
+  const onClick = async () => {
+    if(!ref.current)
+    {
+      return;
+    }
+    else
+    {
+      const result = await ref.current.build({
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false,
+        plugins: [unpkgPathPlugin()]
+      });
+
+    // console.log(result)
+      setCode(result.outputFiles[0].text);
+    }
+    
+  };
+
+  return(
+    <div>
+      <textarea value={input} onChange={(e)=>{setInput(e.target.value)}}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +65,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
