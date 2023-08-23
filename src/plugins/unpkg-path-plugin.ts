14b5cb708b8c3b89329ec782fed18ceea2a86a8a
zakialvi-e64 <zaki.alvi@enterprise64.com>
2023-08-23 18:15:11 +0500

    Done with folder: 08, video: 073

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,23 +1,49 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
+
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
+        if(args.path === 'index.js')
+        {
+          return { path: args.path, namespace: 'a' };
+        }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`
+        };
+
+        // else if (args.path === 'tiny-test-pkg')
+        // {
+        //   return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', 
+        //     namespace: 'a'
+        //   };
+        // }
       });
+
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'tiny-test-pkg';
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };
-        } 
+        }
+        
+        const {data} = await axios.get(args.path);
+        return{
+          loader: 'jsx',
+          contents: data
+        };
+
       });
     },
   };
