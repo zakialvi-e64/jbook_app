4beb29bb3c7bb3fefe1f097304ca5b7fe8cf0d79
zakialvi-e64 <zaki.alvi@enterprise64.com>
2023-08-24 12:27:22 +0500

    folder: 08 video: 077 done

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,24 +6,24 @@ export const unpkgPathPlugin = () => {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
+        console.log('onResolve', args);
         if(args.path === 'index.js')
         {
           return { path: args.path, namespace: 'a' };
+        }
+
+        if(args.path.includes('./') || args.path.includes('../'))
+        {
+          return{
+            namespace: 'a',
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
+          };
         }
 
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
         };
-
-        // else if (args.path === 'tiny-test-pkg')
-        // {
-        //   return {
-        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', 
-        //     namespace: 'a'
-        //   };
-        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -32,16 +32,18 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         }
         
-        const {data} = await axios.get(args.path);
+        const {data, request} = await axios.get(args.path);
+        
         return{
           loader: 'jsx',
-          contents: data
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
         };
 
       });
