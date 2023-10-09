4ba648910c05cf5f62e3919ee1384fc83d029601
zakialvi-e64 <zaki.alvi@enterprise64.com>
2023-10-09 17:57:41 +0500

    [Folder-10] [Completed]

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,9 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 const App = () => {
 
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
+  
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -32,6 +33,8 @@ const App = () => {
     }
     else
     {
+      iframe.current.srcdoc = html;
+
       const result = await ref.current.build({
         entryPoints: ['index.js'],
         bundle: true,
@@ -43,15 +46,30 @@ const App = () => {
         }
       });
 
-      setCode(result.outputFiles[0].text);      
+      iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");      
     }
     
   };
 
   const html = `
-    <script>
-      ${code}
-    </script>
+    <html>
+      <head></head>
+      <body>
+        <div id="root">
+          <script>
+            window.addEventListener('message', (event)=> {
+              try{
+                eval(event.data);
+              } catch(err){
+                const root = document.getElementById('root');
+                root.innerHTML = '<div style="color: red"> <h4>Runtime Error</h4>'+ err +'</div>';
+                console.error(err);
+              }
+            }, false);
+          </script>
+        </div>
+      </body>
+    <html>
   `;
 
   return(
@@ -60,8 +78,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe sandbox="allow-scripts" srcDoc={html}/>
+      <iframe 
+        title="preview" 
+        ref={iframe} 
+        sandbox="allow-scripts" 
+        srcDoc={html}
+      />
     </div>
   );
 }
